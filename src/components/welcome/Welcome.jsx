2093478c3f854b3b9af6de767ee0fb742c66c8a8
Lluben <chayanperu@gmail.com>
2025-06-21 21:05:19 -0500

    enviar el token del usuario final

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
+//user.Nombre lo jala de la api /api/auth/me
 const Welcome = () => {
   const { user } = useSelector((state) => state.auth);
   return (
     <div>
       <h1 className="title">Dashboard</h1>
       <h2 className="subtitle">
-      Bienvenido de nuevo <strong>{user && user.token}</strong>
+      Bienvenido de nuevo <strong>{user && user.Nombre}</strong>
     </h2>
     </div>
   );
